96e434065cfbe3943a7a791a20d02ca0bf468277
LAIBA909352 <sportstorm4@gmail.com>
2026-03-31 09:05:30 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import Loader from "./components/Loader";
+
+export default function App() {
+  return (
+  <>
+  <Loader />
+  </>
+  );
+}
---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+export default function Loader() {
+  return (
+    <>
+    <div className="wrapper">
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+      <span className="dot"></span>
+    </div>
+    </>
+  );
+}
